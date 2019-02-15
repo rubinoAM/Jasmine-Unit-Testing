b7d9a1637a6eabeb313a1a321bd50de87cd99f66
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-15 11:26:17 -0500

    Checks for numeric input that is less than or equal to 7

--- numToWeekday/lib/numToWeekday.js
+++ numToWeekday/lib/numToWeekday.js
@@ -1,5 +1,14 @@
 function numToWeekday(num){
-    return num;
+    if(typeof(num) !== "number"){
+        throw Error("Input must be a number");
+    }
+
+    if(num > 7){
+        throw Error("Input must be between 1 and 7");
+    }
+    else{
+        return num;
+    }
 }
 
 module.exports = numToWeekday;
---
+++ numToWeekday/spec/helpers/numToWeekdaySpec.js
@@ -0,0 +1,15 @@
+describe('Translate a number into a day of the week',()=>{
+    const numToWeekday = require('../../lib/numToWeekday');
+
+    it('should throw error if input is not a number',()=>{
+        expect(()=>{
+            numToWeekday('waffles');
+        }).toThrow(new Error("Input must be a number"))
+    });
+
+    it('should throw error if input > 7',()=>{
+        expect(()=>{
+            numToWeekday(8);
+        }).toThrow(new Error("Input must be between 1 and 7"));
+    })
+});
