89c17969860ba02a0a806ff278061b09b709dc21
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-15 11:36:45 -0500

    Converts num to day of week properly

--- numToWeekday/lib/numToWeekday.js
+++ numToWeekday/lib/numToWeekday.js
@@ -1,4 +1,5 @@
 function numToWeekday(num){
+    //Error Handling
     if(typeof(num) !== "number"){
         throw Error("Input must be a number");
     }
@@ -7,9 +8,16 @@ function numToWeekday(num){
         throw Error("Input must be between 1 and 7");
     } else if(num <= 0){
         throw Error("Input must be between 1 and 7");
-    } else{
-        return num;
     }
+
+    //Return Days
+    if(num == 1){return "Sunday";}
+    else if(num == 2){return "Monday";}
+    else if(num == 3){return "Tuesday";}
+    else if(num == 4){return "Wednesday";}
+    else if(num == 5){return "Thursday";}
+    else if(num == 6){return "Friday";}
+    else if(num == 7){return "Saturday";}
 }
 
 module.exports = numToWeekday;
--- numToWeekday/spec/helpers/numToWeekdaySpec.js
+++ numToWeekday/spec/helpers/numToWeekdaySpec.js
@@ -1,6 +1,7 @@
 describe('Translate a number into a day of the week',()=>{
     const numToWeekday = require('../../lib/numToWeekday');
 
+    //Error Handling
     it('should throw error if input is not a number',()=>{
         expect(()=>{
             numToWeekday('waffles');
@@ -18,4 +19,33 @@ describe('Translate a number into a day of the week',()=>{
             numToWeekday(0);
         }).toThrow(new Error("Input must be between 1 and 7"));
     })
+    
+    //Days
+    it('should return "Sunday" if input == 1',()=>{
+        expect(numToWeekday(1)).toEqual("Sunday");
+    });
+
+    it('should return "Monday" if input == 2',()=>{
+        expect(numToWeekday(2)).toEqual("Monday");
+    });
+
+    it('should return "Tuesday" if input == 3',()=>{
+        expect(numToWeekday(3)).toEqual("Tuesday");
+    });
+
+    it('should return "Wednesday" if input == 4',()=>{
+        expect(numToWeekday(4)).toEqual("Wednesday");
+    });
+
+    it('should return "Thursday" if input == 5',()=>{
+        expect(numToWeekday(5)).toEqual("Thursday");
+    });
+
+    it('should return "Friday" if input == 6',()=>{
+        expect(numToWeekday(6)).toEqual("Friday");
+    });
+
+    it('should return "Saturday" if input == 7',()=>{
+        expect(numToWeekday(7)).toEqual("Saturday");
+    });
 });
