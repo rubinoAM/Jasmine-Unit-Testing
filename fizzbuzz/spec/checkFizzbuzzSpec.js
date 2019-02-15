0f38e0433b85a32fef536f2860a66a8166ed2ef7
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-15 10:15:23 -0500

    Added buzz

--- fizzbuzz/spec/checkFizzbuzzSpec.js
+++ fizzbuzz/spec/checkFizzbuzzSpec.js
@@ -9,3 +9,7 @@ it('should return "2" for 2',()=>{
 it('should return "fizz" for 3',()=>{
     expect(checkFizzbuzz(3)).toEqual('fizz');
 });
+
+it('should return "buzz" for 5',()=>{
+    expect(checkFizzbuzz(5)).toEqual('buzz');
+});
--- fizzbuzz/src/checkFizzbuzz.js
+++ fizzbuzz/src/checkFizzbuzz.js
@@ -1,6 +1,8 @@
 const checkFizzbuzz = (input) => {
     if(input % 3 == 0){
         return 'fizz';
+    } else if(input % 5 == 0){
+        return 'buzz';
     } else {
         return input.toString();
     }
