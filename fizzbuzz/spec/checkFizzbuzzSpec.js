58851db61d7d9cadad06a6428fea5a09b1232750
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-15 10:28:28 -0500

    Removed unneeded it()'s

--- fizzbuzz/spec/checkFizzbuzzSpec.js
+++ fizzbuzz/spec/checkFizzbuzzSpec.js
@@ -1,11 +1,3 @@
-it('should return "1" for 1',()=>{
-    expect(checkFizzbuzz(1)).toEqual('1');
-});
-
-it('should return "2" for 2',()=>{
-    expect(checkFizzbuzz(2)).toEqual('2');
-});
-
 it('should return "fizz" for 3',()=>{
     expect(checkFizzbuzz(3)).toEqual('fizz');
 });
