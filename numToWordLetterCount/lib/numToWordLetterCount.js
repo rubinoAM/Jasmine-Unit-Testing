1e28e9646d8e2594f05ef120110e53a3a67d8175
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-19 10:20:06 -0500

    Successfully converts integers from 0 to 99

--- numToWordLetterCount/lib/numToWordLetterCount.js
+++ numToWordLetterCount/lib/numToWordLetterCount.js
@@ -2,6 +2,8 @@ function numToWordLetterCount(num){
     //Variables
     let numToWord = '';
     const singleDigitInts = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+    const firstDoubleDigitInts = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    const tensInts = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
 
     //Error Handling
     if(typeof(num) !== "number"){
@@ -14,6 +16,18 @@ function numToWordLetterCount(num){
     //Conversion Process
     if(num < 10){
         numToWord = singleDigitInts[num];
+    } 
+    else if (num >= 10 && num < 20){
+        numToWord = firstDoubleDigitInts[num - 10];
+    } 
+    else if (num >= 20 && num < 100){
+        let numOnes = num % 10;
+        let numTens = num - numOnes;
+        numTens = Math.floor(num / 10);
+        numToWord = tensInts[numTens - 2] + "-" +singleDigitInts[numOnes];
+    }
+    else if (num >= 100 && num < 1000){
+        //CODE
     }
 
     return numToWord.length;
--- numToWordLetterCount/spec/helpers/numToWordLetterCountSpec.js
+++ numToWordLetterCount/spec/helpers/numToWordLetterCountSpec.js
@@ -22,4 +22,28 @@ describe('Convert an integer into an English written word and return its letter 
     it('should return 3 if input == 6',()=>{
         expect(numToWordLetterCount(6)).toEqual(3);
     });
+
+    it('should return 6 if input == 12',()=>{
+        expect(numToWordLetterCount(12)).toEqual(6);
+    });
+
+    it('should return 12 if input == 23',()=>{
+        expect(numToWordLetterCount(23)).toEqual(12);
+    });
+
+    /*it('should return 23 if input == 342',()=>{
+        expect(numToWordLetterCount(342)).toEqual(23);
+    });
+
+    it('should return 20 if input == 115',()=>{
+        expect(numToWordLetterCount(115)).toEqual(20);
+    });
+
+    it('should return 24 if input == 999',()=>{
+        expect(numToWordLetterCount(999)).toEqual(24);
+    });
+
+    it('should return 11 if input == 1000',()=>{
+        expect(numToWordLetterCount(1000)).toEqual(11);
+    });*/
 });
